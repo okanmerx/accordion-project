cd49bdf4f5f2d9285dfd3ac68dc0fcb69f1b0d64
Okan <okanmerx@gmail.com>
2024-09-13 15:40:38 -0700

    Accordion-project

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Can you briefly introduce yourself?',
+      info:
+        "Hi, I'm Okan Mersinlioglu, a software engineer living in California. I have 9 years of experience working as a QA engineer at major companies like Meta, Google, and Apple. I also develop projects using React and Django, and I closely follow advancements in the tech industry.",
+    },
+    {
+      id: 2,
+      title: 'Can you tell me about your educational background?',
+      info:
+        'I completed my university education in Turkey, followed by a master’s and PhD in Spain. My PhD research focused on biomedicine, and I worked as a research engineer under a spanish government scholarship while completing my higher doctorate.',
+    },
+    {
+      id: 3,
+      title: 'What is one of the most significant experiences in your career?',
+      info:
+        'Working for companies like Apple, Google, and Meta has been a major highlight. Leading international projects in QA testing and software development has shaped my career. I also have extensive experience testing smart devices and medical devices and working on projects related to Android & iOS devices.',
+    },
+    {
+      id: 4,
+      title: 'What technical skills do you possess?',
+      info:
+        'As a full-stack and frontend developer, I have expertise in HTML, CSS, JavaScript, React, and Node.js. I am also skilled in software testing, test planning, bug detection, and ADB debugging.',
+    },
+    {
+      id: 5,
+      title: 'What do you enjoy doing in your free time?',
+      info:
+        'I’ve been involved in judo since a young age, winning several medals, and participated in scouting camps. Nowadays, I enjoy traveling and discovering new cultures; I’ve had the chance to visit over 50 countries so far.',
+    },
+  ]
+  export default questions
